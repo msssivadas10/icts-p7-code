532d628e43ae6036c4dbef76142d4251b1cbd2e0
ms3 <ms3.msssivadas@gmail.com>
2023-05-02 12:44:11 +0530

    update

---
+++ bt_neighbour.mpi.py
@@ -0,0 +1,11 @@
+import numpy as np
+import astropy.cosmology as ac
+from mpi4py import MPI
+from sklearn.neighbors import BallTree
+from typing import Any 
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size() # get number of processes
+rank = comm.Get_rank() # get the process rank
+
+print(rank)
