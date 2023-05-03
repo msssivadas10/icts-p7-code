1b7ff5e71802821c22cd8ebb7e942303ba530b1a
ms3 <ms3.msssivadas@gmail.com>
2023-05-03 18:59:57 +0530

    added coord transformation

--- kdtreecode.py
+++ kdtreecode.py
@@ -1,6 +1,15 @@
 from scipy.spatial import KDTree
 import numpy as np
 
+def get_xxyyzz_simple(ra, dec):
+    ra = ra*np.pi/180.
+    dec = dec*np.pi/180.
+    xx = np.cos(dec) * np.cos(ra)
+    yy = np.cos(dec) * np.sin(ra)
+    zz = np.sin(dec)
+
+    return xx, yy, zz
+
 def get_xxyyzz(data):
     dec, ra = data.T
     ra = ra*np.pi/180.
@@ -13,22 +22,14 @@ def get_xxyyzz(data):
 
 class BallTree:
 
-    def init(self, data=None):
+    def __init__(self, data=None):
         self.xx, self.yy, self.zz = get_xxyyzz(data)
 
         self.tree = KDTree(np.array([self.xx, self.yy, self.zz]).T)
 
 
-    def query_radius(self, data_src, theta_max, return_distance):
+    def query_radius(self, data_src, theta_max):
     
         xx, yy, zz = get_xxyyzz(data_src)
         idx = self.tree.query_ball_point(np.array([xx, yy, zz]).T, r=theta_max)
-        dist = idx * 1
-        if return_distance:
-            for ii in range(idx.shape[0]):
-                # Get_distances
-                dist[ii] = np.arccos(max(0, min(xx[ii]*xx[idx[ii]] + yy[ii]*yy[idx[ii]] + zz[ii]*zz[idx[ii]], 1)))
-
-            return idx, dist
-
         return idx
